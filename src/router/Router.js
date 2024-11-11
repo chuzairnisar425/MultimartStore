38b5bd384bfdc5a307213606e228310004de70f0
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-11-11 15:17:25 +0500

    modified

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,8 +1,27 @@
+import { Routes, Route } from 'react-router-dom'
+
 import React from 'react'
 
+import Home from '../pages/Home'
+import Shop from '../pages/Shop'
+import Cart from '../pages/Cart'
+import ProductDetails from '../pages/ProductDetails'
+import Checkout from '../pages/Checkout'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
 function Router() {
     return (
-        <div>Router</div>
+        <Routes>
+            <Route path='home' element={<Home />} />
+            <Route path='shop' element={<Shop />} />
+            <Route path='shop/:id' element={<ProductDetails />} />
+            <Route path='checkout' element={<Checkout />} />
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
+            <Route path='cart' element={<Cart />} />
+
+
+        </Routes>
     )
 }
 
